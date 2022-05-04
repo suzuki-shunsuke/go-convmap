32581037da22a8b6465d6d69fb55c14a01a688ee
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-05-04 23:26:59 +0900

    feat: add ConvertMapKey
    BREAKING CHANGE: the signature of Convert is changed

--- convmap/map.go
+++ convmap/map.go
@@ -1,20 +1,25 @@
 package convmap
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 // Convert converts map[interface{}]interface{} to map[string]interface{} in data.
 // data isn't changed.
-// If the type of the key of map isn't string, an error is returned.
-func Convert(data interface{}) (interface{}, error) {
+func Convert(data interface{}, convertMapKey ConvertMapKey) (interface{}, error) {
+	if convertMapKey == nil {
+		convertMapKey = ConvertMapKeySmart
+	}
 	switch t := data.(type) {
 	case map[interface{}]interface{}:
 		m := make(map[string]interface{}, len(t))
 		for k, v := range t {
-			s, ok := k.(string)
-			if !ok {
-				return nil, fmt.Errorf("the map key should be string: %+v", k)
+			s, err := convertMapKey(k)
+			if err != nil {
+				return nil, err
 			}
-			val, err := Convert(v)
+			val, err := Convert(v, convertMapKey)
 			if err != nil {
 				return nil, fmt.Errorf("key: %s: %w", s, err)
 			}
@@ -24,7 +29,7 @@ func Convert(data interface{}) (interface{}, error) {
 	case []interface{}:
 		arr := make([]interface{}, len(t))
 		for i, v := range t {
-			val, err := Convert(v)
+			val, err := Convert(v, convertMapKey)
 			if err != nil {
 				return nil, fmt.Errorf("index: %d: %w", i, err)
 			}
@@ -34,4 +39,34 @@ func Convert(data interface{}) (interface{}, error) {
 	default:
 		return data, nil
 	}
+}
+
+// ConvertMapKey converts map's key interface{} to string.
+type ConvertMapKey func(key interface{}) (string, error)
+
+// ConvertMapKeySmart converts interface{} to string.
+// If key is bool, int, or int64, it is converted to string with strconv package.
+func ConvertMapKeySmart(key interface{}) (string, error) {
+	switch k := key.(type) {
+	case string:
+		return k, nil
+	case bool:
+		return strconv.FormatBool(k), nil
+	case int:
+		return strconv.Itoa(k), nil
+	case int64:
+		return strconv.FormatInt(k, 10), nil //nolint:gomnd
+	default:
+		return "", fmt.Errorf("the map key should be string: %+v", key)
+	}
+}
+
+// ConvertMapKeyStrict converts interface{} to string.
+// If key isn't a string, an error is returned.
+func ConvertMapKeyStrict(key interface{}) (string, error) {
+	s, ok := key.(string)
+	if !ok {
+		return "", fmt.Errorf("the map key should be string: %+v", key)
+	}
+	return s, nil
 }
